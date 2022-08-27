8206cb43d8da4a6f73c43d51dac26ad661b483d7
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2022-08-27 10:09:10 +0530

    updated bash script

--- What'sApp_Boming_Yogesh/bomb.sh
+++ What'sApp_Boming_Yogesh/bomb.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+
+echo "Hello this is YOGESH JOGA CHAT BOMBING TOOL FUN WITH YOUR FRIENDS"
+echo "Enter your name"
+read name
+echo "you need to follow some rules we are working on it actually, give the correct delay time per sec it will send 100 so how may"
+echo "humderads you need to pass the value of hunderads of number"
+echo "If you want to clear screen "
+read num
+if [ $num -gt 0 ]
+then
+		echo "now, clear your screen...."
+		clear
+		java -jar yogeshbomb.jar
+fi
 java -jar yogeshbomb.jar
